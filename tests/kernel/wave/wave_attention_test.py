97b7fc7d8581c715e692c43dff04a6f414017a96
Harsh Menon <harsh@nod-labs.com>
2024-11-13 22:22:34 -0800

    Add sample implementation of decode attention

--- tests/kernel/wave/wave_attention_test.py
+++ tests/kernel/wave/wave_attention_test.py
@@ -517,3 +517,263 @@ def testAttention(
                 f.write(mb.module_op.get_asm())
 
         assert_allclose(output, torch_ref)
+
+
+# Below is a sample implementation of flash decoding in wave.
+@require_e2e
+@pytest.skip("Not implemented yet")
+@pytest.mark.parametrize("shape", get_test_shapes("test_attention"))
+@pytest.mark.parametrize("enable_scheduling", [False])
+@pytest.mark.parametrize(
+    "mfma_variant",
+    [
+        MMAType.F32_16x16x16_F16,
+        MMAType.F32_32x32x8_F16,
+    ],
+)
+def testDecodeAttention(
+    shape: tuple[int], enable_scheduling: bool, mfma_variant: MMAType, request
+):
+    run_bench = request.config.getoption("--runperf")
+    dump_perf = request.config.getoption("--dump-perf-files-path")
+    # Input sizes
+    B = tkl.sym.B
+    Bp = tkl.sym.Bp
+    M = tkl.sym.M
+    N = tkl.sym.N
+    K1 = tkl.sym.K1
+    K2 = tkl.sym.K2
+    H = tkl.sym.H
+    U = tkl.sym.U
+    V = tkl.sym.V
+    # Workgroup tile sizes
+    BLOCK_B = tkl.sym.BLOCK_B
+    BLOCK_H = tkl.sym.BLOCK_H
+    BLOCK_M = tkl.sym.BLOCK_M
+    BLOCK_N = tkl.sym.BLOCK_N
+    BLOCK_K2 = tkl.sym.BLOCK_K2
+    # Address space (for GPU, shared(1) or global(0))
+    ADDRESS_SPACE = tkl.sym.ADDRESS_SPACE
+    # Other hyperparameters
+    LOAD_ELEMS_PER_THREAD = tkl.sym.LOAD_ELEMS_PER_THREAD
+    STORE_ELEMS_PER_THREAD = tkl.sym.STORE_ELEMS_PER_THREAD
+
+    # Expose user-constraints
+    # Distribute batch dimension, head and sequence length along workgroups for first kernel.
+    # The tile size for the batch dimension is 1.
+    constraints: list[tkw.Constraint] = [tkw.WorkgroupConstraint(B, BLOCK_B, 0)]
+    # BLOCK_H could be greater than H.
+    constraints += [tkw.WorkgroupConstraint(H, BLOCK_H, 1)]
+    constraints += [tkw.WorkgroupConstraint(K2, BLOCK_K2, 2)]
+    # Distribute sequence length further among waves.
+    constraints += [tkw.WaveConstraint(K2, BLOCK_K2 / 2)]
+
+    if mfma_variant == MMAType.F32_16x16x16_F16:
+        Mvec = 16
+        Nvec = 16
+    if mfma_variant == MMAType.F32_32x32x8_F16:
+        Mvec = 32
+        Nvec = 32
+
+    constraints += [
+        tkw.HardwareConstraint(
+            threads_per_wave=64,
+            waves_per_block=(2, 2, 1),
+            mma_type=mfma_variant,
+            vector_shapes={B: 0, M: Mvec, N: Nvec},
+        )
+    ]
+
+    # Block table maps from logical to physical partitions and has shape [U, V].
+    i = tkw.IndexMapping.iterator(0)
+    j = tkw.IndexMapping.iterator(1)
+    k = tkw.IndexMapping.iterator(2)
+    block_table_mapping = lambda x: tkw.IndexMapping(
+        num_iterators=3, inputs={B: i, N: j, M: k}, outputs={B: i, M: k, N: j}
+    )
+    # Page table mapping gets offset to load from.
+    page_table_mapping = lambda x: tkw.IndexMapping(
+        num_iterators=3, inputs={B: i, N: j, M: k}, outputs={B: i, M: k, N: j}
+    )
+    output_mapping = lambda x: tkw.IndexMapping(
+        num_iterators=3, inputs={B: i, N: j, M: k}, outputs={B: i, M: k, N: j}
+    )
+
+    # This is the first kernel in flash decode. Here we only compute Q @ K.T.
+    # B: batch size, H: num heads, K1: head dim
+    # Bp: page table size
+    # U, V are the block table dimensions
+    # K2 is the maximum sequence length in the batch
+    @tkw.wave(constraints)
+    def compute_qkt(
+        q: tkl.Memory[B, H, K1, ADDRESS_SPACE, tkl.f16],  # TODO: bf16
+        k_page_table: tkl.Memory[Bp, 1, K1, ADDRESS_SPACE, tkl.f16],  # TODO: bf16
+        req_to_tokens: tkl.Memory[U, V, GLOBAL_ADDRESS_SPACE, tkl.i32],
+        b_req_idx: tkl.Memory[B, GLOBAL_ADDRESS_SPACE, tkl.i32],
+        b_req_start_loc: tkl.Memory[B, GLOBAL_ADDRESS_SPACE, tkl.i32],
+        b_req_seq_len: tkl.Memory[B, GLOBAL_ADDRESS_SPACE, tkl.i32],
+        qkt: tkl.Memory[H, B, K2, GLOBAL_ADDRESS_SPACE, tkl.f16],
+        q_stride: tkl.Memory[2, GLOBAL_ADDRESS_SPACE, tkl.i32],
+        sm_scale: tkl.Memory[1, GLOBAL_ADDRESS_SPACE, tkl.f32],
+        logit_cap: tkl.Memory[1, GLOBAL_ADDRESS_SPACE, tkl.f32],
+    ):
+        c_reg = tkl.Register[H, B, K2, tkl.f32](0.0)
+
+        # We do everything below only if we have not exceeded the sequence length.
+        # Since the sequence length is dynamic, we need to check for this condition
+        # dynamically.
+
+        @tkw.reduction(K1, init_args=[c_reg])
+        def loop(
+            acc: tkl.Register[H, B, K2, tkl.f32]
+        ) -> tkl.Register[H, B, K2, tkl.f32]:
+
+            # Q is loaded from: [w.x, w.y] and has shape [BLOCK_B, BLOCK_H, BLOCK_K1]
+            # But H < BLOCK_H, so we need to apply a mask in the read below so we only read
+            # valid elements.
+            q_reg = tkw.read(q, elements_per_thread=LOAD_ELEMS_PER_THREAD)
+            # k_off is loaded from: [b_req_idx, w.z * BLOCK_K2]
+            k_off = tkw.read(
+                req_to_tokens,
+                mapping=block_table_mapping(b_req_idx, b_req_start_loc, b_req_seq_len),
+                elements_per_thread=1,
+            )
+            # k_reg is loaded from: [k_off, w.y / ceildiv(kv_group_num / BLOCK_H), 1] and has shape [BLOCK_K2, BLOCK_K1]
+            k_reg = tkw.read(
+                k_page_table,
+                mapping=page_table_mapping(k_off),
+                elements_per_thread=LOAD_ELEMS_PER_THREAD,
+            )
+            acc = tkw.mma(k_reg, q_reg, acc)
+            return acc
+
+        acc *= sm_scale
+        acc *= logit_cap * tkw.tanh(acc / logit_cap)
+
+        tkw.write(
+            loop,
+            qkt,
+            mapping=output_mapping,
+            elements_per_thread=STORE_ELEMS_PER_THREAD,
+        )
+
+    @tkw.wave(constraints)
+    def compute_softmax_v(
+        q: tkl.Memory[B, M, K1, ADDRESS_SPACE, tkl.f16],
+        k: tkl.Memory[B, K2, K1, ADDRESS_SPACE, tkl.f16],
+        c: tkl.Memory[B, M, K2, GLOBAL_ADDRESS_SPACE, tkl.f32],
+    ):
+        c_reg = tkl.Register[B, N, M, tkl.f32](0.0)
+        init_sum = tkl.Register[B, M, tkl.f32](0.0)
+        init_max = tkl.Register[B, M, tkl.f32](-1e6)
+
+        # This microkernel encodes the fact that if the reduction
+        # dimension were tiled, then we would need to materialize a loop.
+        @tkw.reduction(K2, init_args=[init_max, init_sum, c_reg])
+        def repeat(
+            partial_max: tkl.Register[B, M, tkl.f32],
+            partial_sum: tkl.Register[B, M, tkl.f32],
+            acc: tkl.Register[B, N, M, tkl.f32],
+        ) -> (
+            tkl.Register[B, M, tkl.f32],
+            tkl.Register[B, M, tkl.f32],
+            tkl.Register[B, N, M, tkl.f32],
+        ):
+            imm_reg = tkl.Register[B, K2, M, tkl.f32](0.0)
+            q_reg = tkw.read(q, elements_per_thread=LOAD_ELEMS_PER_THREAD)
+            # b_reg: tkw.Register[B, N, K, tkl.f16]
+            k_reg = tkw.read(k, elements_per_thread=LOAD_ELEMS_PER_THREAD)
+            # acc: tkw.Register[B, N, M, tkl.f32]
+            inner_acc = tkw.mma(k_reg, q_reg, imm_reg)
+            x_j = tkw.permute(inner_acc, target_shape=[B, M, K2])
+            m_j = tkw.max(x_j, partial_max, dim=K2)
+            e_delta_max = tkw.exp2(partial_max - m_j)
+            e_delta = tkw.exp2(x_j - m_j)
+            e_init = partial_sum * e_delta_max
+            d_j = tkw.sum(e_delta, e_init, dim=K2)
+            imm_f16 = tkw.cast(e_delta, tkl.f16)
+            v_reg = tkw.read(v, elements_per_thread=LOAD_ELEMS_PER_THREAD)
+            new_acc = acc * e_delta_max
+            acc = tkw.mma(v_reg, imm_f16, new_acc)
+            return m_j, d_j, acc
+
+        # repeat represents the results of the loop
+        res_max, res_sum, res_mm = repeat
+        res = res_mm / res_sum
+        tkw.write(res, c, mapping=mapping, elements_per_thread=STORE_ELEMS_PER_THREAD)
+
+    hyperparams = {
+        ADDRESS_SPACE: SHARED_ADDRESS_SPACE,
+        LOAD_ELEMS_PER_THREAD: get_mfma_load_elems_per_thread(mfma_variant),
+        STORE_ELEMS_PER_THREAD: get_mfma_store_elems_per_thread(mfma_variant),
+        BLOCK_B: 1,
+        BLOCK_M: 64,
+        BLOCK_N: 64,
+        BLOCK_K2: 64,
+        B: shape[0],
+        M: shape[1],
+        N: shape[2],
+        K1: shape[3],
+        K2: shape[4],
+        READ_SHARED_DELAY: 1,
+        WRITE_SHARED_DELAY: 1,
+        READ_GLOBAL_DELAY: 2,
+        WRITE_GLOBAL_DELAY: 2,
+        MMA_DELAY: 1,
+        SHARED_MEMORY_UNITS: 4,
+        GLOBAL_MEMORY_UNITS: 4,
+        MMA_UNITS: 4,
+    }
+    config = {"backend": "rocm", "device": "hip", "target": "gfx942"}
+    if run_bench:
+        config["benchmark_batch_size"] = 10
+        config["benchmark_repetitions"] = 3
+    if dump_perf is not None:
+        perf_filename = request.node.name + ".json"
+        config["benchmark_results_file"] = os.path.join(
+            dump_perf, "tk_" + perf_filename
+        )
+
+    with tk.gen.TestLaunchContext(
+        hyperparams,
+        canonicalize=True,
+        run=True,
+        run_bench=run_bench,
+        run_config=config,
+        schedule=enable_scheduling,
+        use_scheduling_barriers=enable_scheduling_barriers,
+    ):
+        torch.manual_seed(0)
+        q = torch.randn(shape[0], shape[1], shape[3], dtype=torch.float16)
+        k = torch.randn(shape[0], shape[4], shape[3], dtype=torch.float16)
+        v = torch.randn(shape[0], shape[4], shape[2], dtype=torch.float16)
+        output = torch.zeros(shape[0], shape[1], shape[2], dtype=torch.float32)
+        log2e = 1.44269504089
+        dk_sqrt = math.sqrt(1.0 / shape[3])
+        # TODO: Add scaling of QK as part of kernel.
+        # TODO: Add variant of non-transposed V attention kernel.
+        # mb = base_attention(q * dk_sqrt * log2e, k, v.permute([0, 2, 1]), output)
+
+        compute_qkt(
+            q, k_page_table, req_to_token, b_req_idx, b_start_loc, b_seq_len, qk_out
+        )
+        compute_softmax_v(
+            qk_out,
+            v_page_table,
+            req_to_token,
+            b_req_idx,
+            b_start_loc,
+            b_seq_len,
+            output,
+        )
+
+        torch_ref = torch.nn.functional.scaled_dot_product_attention(
+            q, k, v, attn_mask=None
+        )
+
+        if test_dump_generated_mlir:
+            filename = f"wave_attention_{'x'.join(map(str, shape))}.mlir"
+            with open(filename, "w") as f:
+                f.write(mb.module_op.get_asm())
+
+        assert_allclose(output, torch_ref)
